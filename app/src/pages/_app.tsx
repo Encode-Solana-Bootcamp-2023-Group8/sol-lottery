8ef567c3b61094f582c345c35a1758151cc95628
tben140 <tben140@gmail.com>
2023-10-03 17:41:33 +0100

    ADD startgame button component

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -6,6 +6,8 @@ import { AppBar } from '../components/AppBar';
 import { ContentContainer } from '../components/ContentContainer';
 import { Footer } from '../components/Footer';
 import Notifications from '../components/Notification'
+import { RequestAirdrop } from 'components/RequestAirdrop';
+import { StartGame } from 'components/StartGame';
 require('@solana/wallet-adapter-react-ui/styles.css');
 require('../styles/globals.css');
 
@@ -20,10 +22,14 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
             <div className="flex flex-col h-screen">
               <Notifications />
               <AppBar/>
-              <ContentContainer>
-                <Component {...pageProps} />
+              <div class="flex justify-center mt-40">
+                <StartGame />
+                <RequestAirdrop />
+              </div>
+              {/* <ContentContainer> */}
+                {/* <Component {...pageProps} /> */}
                 {/* <Footer/> */}
-              </ContentContainer>
+              {/* </ContentContainer> */}
             </div>
           </ContextProvider>
         </>
